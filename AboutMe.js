b3fb3101d5f4fbc03fc7f637ee8e85cc4bfaee4d
Brittani <brittani.morganfield@gmail.com>
2021-07-24 12:34:28 -0500

    java

---
+++ AboutMe.js
@@ -0,0 +1,21 @@
+// ask user their name
+let giveName = prompt("What's your name?");
+
+// are you sure?
+let areYouSure = confirm("Get ready for the guessing game, " + giveName + "!");
+
+// ask abt jobs
+let guessJobs = true;
+while (guessJobs) {
+let guessJobs = prompt("Has Brittani worked 3 jobs?");
+
+{
+    if (response === "yes") {
+        guessJobs = false;
+    
+    alert("You're right!"); 
+     } else {
+    alert("Not quite... Try again!"); 
+    }
+}
+}
